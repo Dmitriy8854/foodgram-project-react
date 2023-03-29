a01e55e22552ee9844dcc7c10fd30348dfe3b882
Морозов Дмитрий <dmit.morozoff2009@yandex.ru>
2023-03-29 22:52:32 +0300

    ы

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -81,7 +81,7 @@ class RecipeViewSet(ModelViewSet):
                 status=HTTP_400_BAD_REQUEST
             )
         ingredients = RecipeIngredient.objects.filter(
-            recipes__baskets__user=request.user
+            recipe__baskets__user=request.user
         ).values(
             'ingredient__name',
             'ingredient__measurement_unit'
