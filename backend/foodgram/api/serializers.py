2349cde486aacc58510c73e2c338699d27f87548
Морозов Дмитрий <dmit.morozoff2009@yandex.ru>
2023-05-02 16:56:34 +0300

    fixed serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -36,7 +36,7 @@ class IngredientRecipeSerializer(ModelSerializer):
         model = RecipeIngredient
         fields = ('id', 'name', 'measurement_unit', 'amount')
 
-
+"""Class Base64Image."""
 class Base64ImageField(ImageField):
     def to_internal_value(self, data):
         if isinstance(data, str) and data.startswith('data:image'):
